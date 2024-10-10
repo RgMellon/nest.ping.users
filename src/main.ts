6ac5eb0c7b8ba8830d4c09db264a3656299c81fa
admin <admin@Renan.local>
2024-10-10 19:32:54 -0300

    feat: add prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = new ConfigService();
+
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        `amqp://${configService.get<string>('RABBITMQ_USER')}:${configService.get<string>('RABBITMQ_PASSWORD')}@${configService.get<string>('RABBITMQ_URL')}`,
+      ],
+      noAck: false, // acknolage, so pode deletar a msg da fila depois que devolver uma msg pra ele
+      queue: 'admin-backend',
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
